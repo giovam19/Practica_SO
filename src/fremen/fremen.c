920637b7cff208db352110395b74f7deeb57ef10
giovam19 <giovanni.vechies@students.salle.url.edu>
2021-10-10 21:18:46 +0200

    fremen hecho falta separar argumentos

--- src/fremen/fremen.c
+++ src/fremen/fremen.c
@@ -1,15 +1,82 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/un.h>
+#include <sys/socket.h>
+#include <strings.h>
+#include <ctype.h>
 #include "ConfigFichero.h"
 
+// hacer funcion separa argumentos por espacios.
+
+char *comandoToMinus(char *input) {
+    char *comandoaux = (char *) malloc(sizeof(char));
+    int i;
+
+    for (i = 0; input[i] != ' ' && input[i] != '\0'; i++) {
+        comandoaux = (char *) realloc(comandoaux, sizeof(char) * (i + 2));
+        comandoaux[i] = tolower(input[i]);
+    }
+
+    comandoaux[i] = '\0';
+
+    return comandoaux;
+}
+
+void comandoLinux(char *comando) {
+    char **argumento;
+    int pid;
+
+    pid = fork();
+
+    if (pid == 0) {
+        int status = execvp(comando, argumento);
+
+        if (status == -1) {
+            printf("error execvp\n");
+        }
+    } else {
+        wait(NULL);
+    }
+}
+
+void menuComandos(char *input) {
+    char *comando = comandoToMinus(input);
+
+    printf("%s\n", comando);
+
+    if (strcmp(comando, "login") == 0) {
+        write(1, "login ok\n", 9);
+    } else if (strcmp(comando, "search") == 0) {
+        write(1, "search ok\n", 10);
+    } else if (strcmp(comando, "send") == 0) {
+        write(1, "send ok\n", 10);
+    } else if (strcmp(comando, "photo") == 0) {
+        write(1, "photo ok\n", 10);
+    } else if (strcmp(comando, "logout") == 0) {
+        write(1, "logout ok\n", 10);
+    } else {
+        write(1, "error\n", 6);
+        comandoLinux(comando);
+    }
+}
+
 int main() {
-    Configuracion datos;
+    //Configuracion datos;
+    char input[20];
 
-	datos = leerFichero("config.dat");
+	//datos = leerFichero("config.dat");
 
-	printf("%s\n", datos.ip);
-    printf("%s\n", datos.directorio);
-    
-    printf("Hola Mundo\n");
+    while(1) {
+        bzero(input, strlen(input));
+        read(0, input, 20);
+        input[strlen(input)-1] = '\0';
+        
+        menuComandos(input);
+    }
     
     return 0;
 }
